9d75bfb3a573bff55a61265a607880543abbcea0
Nikhil-KIET <nikhil.2024cse1206@kiet.edu>
2026-06-24 22:41:58 +0530

    Added project files

---
+++ src/test/java/com/example/VIBER/VIBER/ViberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VIBER.VIBER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
