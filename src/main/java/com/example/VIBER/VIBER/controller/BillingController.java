c11886b17f8305a258a63fd4e52bd91c68238f69
Nikhil-KIET <nikhil.2024cse1206@kiet.edu>
2026-07-02 01:49:52 +0530

    Billing controller Added

--- src/main/java/com/example/VIBER/VIBER/controller/BillingController.java
+++ src/main/java/com/example/VIBER/VIBER/controller/BillingController.java
@@ -1,20 +1,41 @@
 package com.example.VIBER.VIBER.controller;
 
-import com.example.VIBER.VIBER.dto.billing.PlanResponse;
+import com.example.VIBER.VIBER.dto.billing.*;
+import com.example.VIBER.VIBER.service.BillingService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/billing")
 @RequiredArgsConstructor
 public class BillingController {
 
+    private final BillingService billingService;
     @GetMapping("/plans")
     public ResponseEntity<PlanResponse>getPlans(){
 
+        return  ResponseEntity.ok(billingService.getPlans());
+
+    }
+
+    @GetMapping("/subscription")
+    public ResponseEntity<SubscriptionResponse>getSubscription(){
+        return  ResponseEntity.ok(billingService.getSubscription());
+    }
+    @PostMapping("/stripe/checkout")
+    public ResponseEntity<CheckoutResponse> createCheckoutResponse(
+            @RequestBody CheckoutRequest request
+    ) {
+        Long userId = 1L;
+        return ResponseEntity.ok(billingService.createCheckoutSessionUrl(request, userId));
+    }
+
+    @PostMapping("/stripe/portal")
+    public ResponseEntity<PortalResponse> openCustomerPortal() {
+        Long userId = 1L;
+        return ResponseEntity.ok(billingService.openCustomerPortal(userId));
     }
 
 }
--- src/main/java/com/example/VIBER/VIBER/service/BillingService.java
+++ src/main/java/com/example/VIBER/VIBER/service/BillingService.java
@@ -1,4 +1,15 @@
 package com.example.VIBER.VIBER.service;
 
+import com.example.VIBER.VIBER.dto.billing.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.ProblemDetail;
+
 public interface BillingService {
+    PlanResponse getPlans();
+
+     SubscriptionResponse getSubscription();
+
+     CheckoutResponse createCheckoutSessionUrl(CheckoutRequest request, Long userId);
+
+     PortalResponse openCustomerPortal(Long userId);
 }
